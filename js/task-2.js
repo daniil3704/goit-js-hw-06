380ad7e8eb24fd1f82d49b753d2b889a8030862e
Даниил Кузнецов <daniil190175@gmail.com>
2024-08-26 17:40:06 +0300

    Final commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,45 +1,35 @@
-function calcAverageCalories(days) {
-    let totalCalories = 0
-    let averageCalories = 0
-    for (const day of days){
-        totalCalories += day.calories
-    }
+class Storage{
+  #items = []
 
-    averageCalories = totalCalories/7
+  constructor(items){
+    this.#items = items
+  }
 
-    if (averageCalories > 0){
-        return averageCalories
-    }
-    else{
-        return 0
-    }
+  getItems(){
+    return this.#items
+  }
 
+  addItem(newItem){
+    this.#items.push(newItem)
+  }
+
+  removeItem(itemToRemove){
+    let index = this.#items.indexOf(itemToRemove)
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+  }
+}
 }
 
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-  ); // 3180
-  
-  console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-  ); // 2270
-  
-  console.log(
-    calcAverageCalories([])
-  ); // 0
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,33 @@
-const profile = {
-    username: "Jacob",
-  playTime: 300,
+class StringBuilder{
+  #value;
 
-  changeUsername(newName){
-     this.username = newName
-  },
-  updatePlayTime(hours){
-    this.playTime += hours
-  },
-  getInfo(){
-    return `${this.username} has ${this.playTime} active hours!`
+  constructor(initialValue){
+    this.#value = initialValue
   }
-  
-};
 
+  getValue(){
+    return this.#value
+  }
 
+  padEnd(str){
+    this.#value += str
+  }
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  padStart(str){
+    this.#value = str + this.#value
+  }
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  padBoth(str){
+    this.#value = str + this.#value + str
+  }
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
